92a0b654d6d34eb2990a32eff5693a58cc48cc80
Mark J Crane <markjcrane@fusionpbx.com>
2010-06-16 04:58:05 +0000

    PHP Service allows the creation of one or more services written in PHP. It opens up many possibilities such as monitoring services, remote systems, ssh logs, drive space alerts and more.
    git-svn-id: http://fusionpbx.googlecode.com/svn/trunk@244 e2954ef0-caed-11de-bd98-e341d93cc3cc

--- fusionpbx/includes/v_version.php
+++ fusionpbx/includes/v_version.php
@@ -25,7 +25,7 @@
 */
 
 $softwarename = 'FusionPBX';
-$softwareversion = '1.1.9';
+$softwareversion = '1.1.10';
 $softwareurl = 'http://www.fusionpbx.com';
 
 ?>
---
+++ fusionpbx/mod/php_service/v_php_service_delete.php
@@ -0,0 +1,56 @@
+<?php
+require_once "root.php";
+require_once "includes/config.php";
+require_once "includes/checkauth.php";
+if (ifgroup("superadmin")) {
+	//access granted
+}
+else {
+	echo "access denied";
+	exit;
+}
+
+if (count($_GET)>0) {
+	$id = check_str($_GET["id"]);
+}
+
+if (strlen($id)>0) {
+	$sql = "";
+	$sql .= "select * from v_php_service ";
+	$sql .= "where php_service_id = '$id' ";
+	$prepstatement = $db->prepare(check_sql($sql));
+	$prepstatement->execute();
+	$result = $prepstatement->fetchAll();
+	foreach ($result as &$row) {
+		$service_name = $row["service_name"];
+		$tmp_service_name = str_replace(" ", "_", $service_name);
+		break; //limit to 1 row
+	}
+	unset ($prepstatement, $result, $row);
+
+	//delete the php service file
+		unlink($v_secure.'/php_service_'.$tmp_service_name.'.php');
+	//delete the start up script
+		unlink($v_startup_script_dir.'/php_service_'.$tmp_service_name.'.sh');
+	//delete the pid file
+		unlink($tmp_dir.'/php_service_'.$tmp_service_name.'.pid');
+
+	$sql = "";
+	$sql .= "delete from v_php_service ";
+	$sql .= "where php_service_id = '$id' ";
+	$prepstatement = $db->prepare(check_sql($sql));
+	$prepstatement->execute();
+	unset($sql);
+}
+
+require_once "includes/header.php";
+echo "<meta http-equiv=\"refresh\" content=\"2;url=v_php_service.php\">\n";
+echo "<div align='center'>\n";
+echo "Delete Complete\n";
+echo "</div>\n";
+
+require_once "includes/footer.php";
+return;
+
+?>
+
