ede89c7639e608d1848ab3b1929a50bdb435218c
Mark J Crane <markjcrane@fusionpbx.com>
2010-07-29 08:53:28 +0000

    Add recordings.lua with ability to preview and approve recordings or re-record. disa.lua use new pin number method which simplifies the code.
    git-svn-id: http://fusionpbx.googlecode.com/svn/trunk@299 e2954ef0-caed-11de-bd98-e341d93cc3cc

--- fusionpbx/includes/install/scripts/disa.lua
+++ fusionpbx/includes/install/scripts/disa.lua
@@ -17,25 +17,14 @@
 --	The Initial Developer of the Original Code is
 --	Mark J Crane <markjcrane@fusionpbx.com>
 --	Copyright (C) 2008-2010
---	All Rights Reserved.
+--	the Initial Developer. All Rights Reserved.
 --
 --	Contributor(s):
 --	Mark J Crane <markjcrane@fusionpbx.com>
 
 predefined_destination = "";
-pin_digit_min_length = "";
-digit_min_length = "2";
-digit_max_length = "11";
-retries = "3";
+max_tries = "3";
 digit_timeout = "5000";
-
---freeswitch.consoleLog("NOTICE", "DISA:\n");
-
---function HangupHook(s, status, arg)
-	--session:execute("info", "");
-	--freeswitch.consoleLog("NOTICE", "HangupHook: " .. status .. "\n");
---end
---session:setHangupHook("HangupHook", "");
 
 if ( session:ready() ) then
 	session:answer( );
@@ -44,46 +33,60 @@ if ( session:ready() ) then
 	caller_id_name = session:getVariable("caller_id_name");
 	caller_id_number = session:getVariable("caller_id_number");
 	predefined_destination = session:getVariable("predefined_destination");
+	digit_min_length = session:getVariable("digit_min_length");
+	digit_max_length = session:getVariable("digit_max_length");
 
-	-- if a pin number has been defined then request the pin number
-	if (pin_number) then
-		pin_length = string.len(pin_number);
-		digits = session:playAndGetDigits(pin_length, pin_length, retries, 3000, "#", sounds_dir.."/custom/please_enter_the_pin_number.wav", "", "\\d+");
-		if (digits == pin_number) then
-			--pin is correct 
-			--session:execute("set", "hangup_after_bridge=true");
-			--session:execute("set", "continue_on_fail=true");
+	--set defaults
+		if (digit_min_length) then
+			--do nothing
+		else
+			digit_min_length = "2";
+		end
 
-			if (predefined_destination) then
-				session:execute("transfer", predefined_destination .. " XML default");
+		if (digit_max_length) then
+			--do nothing
+		else
+			digit_max_length = "11";
+		end
+
+	--if the pin number is provided then require it
+		if (pin_number) then
+			min_digits = string.len(pin_number);
+			max_digits = string.len(pin_number)+1;
+			digits = session:playAndGetDigits(min_digits, max_digits, max_tries, digit_timeout, "#", sounds_dir.."/custom/please_enter_the_pin_number.wav", "", "\\d+");
+			if (digits == pin_number) then
+				--pin is correct
 			else
-				dtmf = ""; --clear dtmf digits to prepare for next dtmf request
-				destination_number = session:playAndGetDigits(digit_min_length, digit_max_length, retries, digit_timeout, "#", sounds_dir.."/custom/please_enter_the_phone_number.wav", "", "\\d+");
-				--if (string.len(destination_number) == 10) then destination_number = "1"..destination_number; end
-				session:execute("transfer", destination_number .. " XML default");
+				session:streamFile(sounds_dir.."/custom/your_pin_number_is_incorect_goodbye.wav");
+				session:hangup("NORMAL_CLEARING");
+				return;
+			end
+		end
+
+	--if a predefined_destination is provided then send the call there otherwise prompt for the destination number then send the call
+		if (predefined_destination) then
+			session:execute("transfer", predefined_destination .. " XML default");
+		else
+			dtmf = ""; --clear dtmf digits to prepare for next dtmf request
+			destination_number = session:playAndGetDigits(digit_min_length, digit_max_length, max_tries, digit_timeout, "#", sounds_dir.."/custom/please_enter_the_phone_number.wav", "", "\\d+");
+			--if (string.len(destination_number) == 10) then destination_number = "1"..destination_number; end
+			session:execute("transfer", destination_number .. " XML default");
+
+			--alternate method
+				--session:execute("set", "hangup_after_bridge=true");
+				--session:execute("set", "continue_on_fail=true");
 				--session:execute("bridge", "sofia/gateway/flowroute.com/"..destination_number);
 
+			--alternate method
 				--local session2 = freeswitch.Session("{ignore_early_media=true}sofia/gateway/flowroute.com/"..destination_number);
 				--t1 = os.date('*t');
 				--call_start_time = os.time(t1);
 				--freeswitch.bridge(session, session2);
-			end
-
-		else 
-			session:streamFile( sounds_dir.."/custom/your_pin_number_is_incorect_goodbye.wav");
-			session:hangup();
 		end
-
-	else
-		--session:execute("set", "hangup_after_bridge=true");
-		--session:execute("set", "continue_on_fail=true");
-
-		if (predefined_destination) then
-				session:execute("transfer", predefined_destination .. " XML default");
-		else
-			dtmf = ""; --clear dtmf digits to prepare for next dtmf request
-			digits = session:playAndGetDigits(digit_min_length, digit_max_length, retries, digit_timeout, "#", sounds_dir.."/custom/please_enter_the_phone_number.wav", "", "\\d+");
-			session:execute("transfer", digits .. " XML default");
-		end
-	end
 end
+
+--function HangupHook(s, status, arg)
+	--session:execute("info", "");
+	--freeswitch.consoleLog("NOTICE", "HangupHook: " .. status .. "\n");
+--end
+--session:setHangupHook("HangupHook", "");
