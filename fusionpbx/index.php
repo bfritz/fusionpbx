44225b4d920b5054cee131f7467170523d85d422
Mark J Crane <markjcrane@fusionpbx.com>
2010-09-09 05:55:27 +0000

    minor version increment
    git-svn-id: http://fusionpbx.googlecode.com/svn/trunk@398 e2954ef0-caed-11de-bd98-e341d93cc3cc

--- fusionpbx/index.php
+++ fusionpbx/index.php
@@ -81,7 +81,7 @@ echo "<br />";
 
 //get the software information
 	//include "includes/v_version.php";
-	$software_version = "1.1.33";
+	$software_version = "1.1.34";
 	echo "<table width=\"100%\" border=\"0\" cellpadding=\"6\" cellspacing=\"0\">\n";
 	echo "<tr>\n";
 	echo "	<th class='th' colspan='2' align='left'>System Information</th>\n";
