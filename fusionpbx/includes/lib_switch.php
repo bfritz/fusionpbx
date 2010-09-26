5aa8f4a9efab7d1cb1b6657f902d470e8f19fa59
Mark J Crane <markjcrane@fusionpbx.com>
2010-09-26 07:03:14 +0000

    Find a better home for the php http compression.
    git-svn-id: http://fusionpbx.googlecode.com/svn/trunk@454 e2954ef0-caed-11de-bd98-e341d93cc3cc

--- fusionpbx/includes/lib_switch.php
+++ fusionpbx/includes/lib_switch.php
@@ -42,6 +42,29 @@ $v_id = '1';
 	$v_fax_show = true;
 	$v_path_show = true;
 
+//determine whether to use php http compression
+	if (strlen($_SESSION['php_http_compression']) == 0) {
+		$sql = "";
+		$sql .= "select * from v_vars ";
+		$sql .= "where v_id = '$v_id' ";
+		$sql .= "and var_name = 'php_http_compression' ";
+		$prepstatement = $db->prepare(check_sql($sql));
+		$prepstatement->execute();
+		$result = $prepstatement->fetchAll();
+		foreach ($result as &$row) {
+			$_SESSION['php_http_compression'] = $row["var_value"];
+			break; //limit to 1 row
+		}
+	}
+//set http compression
+	if ($_SESSION['http_compression'] != "false") {
+			if (substr_count($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip')) {
+				ob_start("ob_gzhandler");
+			}
+			else{
+				ob_start();
+			}
+	}
 
 function v_settings()
 {
