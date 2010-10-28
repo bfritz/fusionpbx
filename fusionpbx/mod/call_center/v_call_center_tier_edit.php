429f630d2d21066cd84f740e96c2c6f7de408f10
Mark J Crane <markjcrane@fusionpbx.com>
2010-10-28 22:08:10 +0000

    Correct the field in the where clause for the Agent select list.
    git-svn-id: http://fusionpbx.googlecode.com/svn/trunk@517 e2954ef0-caed-11de-bd98-e341d93cc3cc

--- fusionpbx/mod/call_center/v_call_center_tier_edit.php
+++ fusionpbx/mod/call_center/v_call_center_tier_edit.php
@@ -180,7 +180,7 @@ if (count($_POST)>0 && strlen($_POST["persistformvar"]) == 0) {
 	//---- Begin Select List --------------------
 	$sql = "SELECT * FROM v_users ";
 	$sql .= "where v_id = '$v_id' ";
-	$sql .= "order by agent_name asc ";
+	$sql .= "order by username asc ";
 	$prepstatement = $db->prepare(check_sql($sql));
 	$prepstatement->execute();
 
