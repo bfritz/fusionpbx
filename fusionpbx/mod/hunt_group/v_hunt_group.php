4065ceee82add377af937573eff236e48f1d2a32
Mark J Crane <markjcrane@fusionpbx.com>
2010-11-25 11:07:54 +0000

    Add hunt group call forward feature requested by a company for use with temporarily redirecting the hunt group to an on call cell phone number.
    git-svn-id: http://fusionpbx.googlecode.com/svn/trunk@595 e2954ef0-caed-11de-bd98-e341d93cc3cc

--- fusionpbx/mod/hunt_group/v_hunt_group.php
+++ fusionpbx/mod/hunt_group/v_hunt_group.php
@@ -38,146 +38,122 @@ require_once "includes/paging.php";
 
 $orderby = $_GET["orderby"];
 $order = $_GET["order"];
-if (!function_exists('thorderby')) {
-	//html table header order by
-	function thorderby($fieldname, $columntitle, $orderby, $order) {
 
-		$html .= "<th class='' nowrap>&nbsp; &nbsp; ";
-		if (strlen($orderby)==0) {
-		  $html .= "<a href='?orderby=$fieldname&order=desc' title='ascending'>$columntitle</a>";
-		}
-		else {
-		  if ($order=="asc") {
-			  $html .= "<a href='?orderby=$fieldname&order=desc' title='ascending'>$columntitle</a>";
-		  }
-		  else {
-			  $html .= "<a href='?orderby=$fieldname&order=asc' title='descending'>$columntitle</a>";
-		  }
-		}
-		$html .= "&nbsp; &nbsp; </th>";
+echo "<div align='center'>";
+echo "<table width='100%' border='0' cellpadding='0' cellspacing='0'>\n";
+echo "<tr class='border'>\n";
+echo "	<td align=\"center\">\n";
+echo "      <br />";
 
-		return $html;
-	}
+echo "<table width='100%' border='0' cellpadding='0' cellspacing='0'>\n";
+echo "<tr>\n";
+echo "    <td align='left'><p><span class='vexpl'><span class='red'><strong>Hunt Group<br />\n";
+echo "        </strong></span>\n";
+echo "			A Hunt Group is a list of destinations that can be called in sequence or simultaneously.\n";
+echo "        </span></p></td>\n";
+echo "</tr>\n";
+echo "</table>\n";
+echo "<br />\n";
+
+
+$sql = "";
+$sql .= " select * from v_hunt_group ";
+$sql .= "where v_id = '$v_id' ";
+if (strlen($orderby)> 0) { $sql .= "order by $orderby $order "; }
+$prepstatement = $db->prepare(check_sql($sql));
+$prepstatement->execute();
+$result = $prepstatement->fetchAll();
+$numrows = count($result);
+unset ($prepstatement, $result, $sql);
+
+$rowsperpage = 150;
+$param = "";
+$page = $_GET['page'];
+if (strlen($page) == 0) { $page = 0; $_GET['page'] = 0; } 
+list($pagingcontrols, $rowsperpage, $var3) = paging($numrows, $param, $rowsperpage); 
+$offset = $rowsperpage * $page; 
+$sql = "";
+$sql .= " select * from v_hunt_group ";
+$sql .= "where v_id = '$v_id' ";
+if (strlen($orderby)> 0) { $sql .= "order by $orderby $order "; }
+$sql .= " limit $rowsperpage offset $offset ";
+$prepstatement = $db->prepare(check_sql($sql));
+$prepstatement->execute();
+$result = $prepstatement->fetchAll();
+$resultcount = count($result);
+unset ($prepstatement, $sql);
+
+
+$c = 0;
+$rowstyle["0"] = "rowstyle0";
+$rowstyle["1"] = "rowstyle1";
+
+echo "<div align='center'>\n";
+echo "<table width='100%' border='0' cellpadding='0' cellspacing='0'>\n";
+
+echo "<tr>\n";
+echo thorderby('huntgroupextension', 'Extension', $orderby, $order);
+echo thorderby('huntgroupname', 'Hunt Group Name', $orderby, $order);
+echo thorderby('huntgroupname', 'Enabled', $orderby, $order);
+echo thorderby('huntgroupdescr', 'Description', $orderby, $order);
+echo "<td align='right' width='42'>\n";
+echo "	<a href='v_hunt_group_edit.php' alt='add'><img src='".$v_icon_add."' width='17' height='17' border='0' alt='add'></a>\n";
+echo "</td>\n";
+echo "<tr>\n";
+
+if ($resultcount == 0) { //no results
 }
-//example use
-
-	echo "<div align='center'>";
-	echo "<table width='100%' border='0' cellpadding='0' cellspacing='0'>\n";
-
-	echo "<tr class='border'>\n";
-	echo "	<td align=\"center\">\n";
-	echo "      <br />";
-
-	
-	echo "<table width='100%' border='0' cellpadding='0' cellspacing='0'>\n";
-	echo "<tr>\n";
-	echo "    <td align='left'><p><span class='vexpl'><span class='red'><strong>Hunt Group<br />\n";
-	echo "        </strong></span>\n";
-	echo "			A Hunt Group is a list of destinations that can be called in sequence or simultaneously.\n";
-	echo "        </span></p></td>\n";
-	echo "</tr>\n";
-	echo "</table>\n";
-	echo "<br />\n";
-
-
-	$sql = "";
-	$sql .= " select * from v_hunt_group ";
-	$sql .= "where v_id = '$v_id' ";
-	if (strlen($orderby)> 0) { $sql .= "order by $orderby $order "; }
-	$prepstatement = $db->prepare(check_sql($sql));
-	$prepstatement->execute();
-	$result = $prepstatement->fetchAll();
-	$numrows = count($result);
-	unset ($prepstatement, $result, $sql);
-
-	$rowsperpage = 150;
-	$param = "";
-	$page = $_GET['page'];
-	if (strlen($page) == 0) { $page = 0; $_GET['page'] = 0; } 
-	list($pagingcontrols, $rowsperpage, $var3) = paging($numrows, $param, $rowsperpage); 
-	$offset = $rowsperpage * $page; 
-	$sql = "";
-	$sql .= " select * from v_hunt_group ";
-	$sql .= "where v_id = '$v_id' ";
-	if (strlen($orderby)> 0) { $sql .= "order by $orderby $order "; }
-	$sql .= " limit $rowsperpage offset $offset ";
-	$prepstatement = $db->prepare(check_sql($sql));
-	$prepstatement->execute();
-	$result = $prepstatement->fetchAll();
-	$resultcount = count($result);
-	unset ($prepstatement, $sql);
-
-
-	$c = 0;
-	$rowstyle["0"] = "rowstyle0";
-	$rowstyle["1"] = "rowstyle1";
-
-	echo "<div align='center'>\n";
-	echo "<table width='100%' border='0' cellpadding='0' cellspacing='0'>\n";
-
-	echo "<tr>\n";
-	echo thorderby('huntgroupextension', 'Extension', $orderby, $order);
-	echo thorderby('huntgroupname', 'Hunt Group Name', $orderby, $order);
-	echo thorderby('huntgroupname', 'Enabled', $orderby, $order);
-	echo thorderby('huntgroupdescr', 'Description', $orderby, $order);
-	echo "<td align='right' width='42'>\n";
-	echo "	<a href='v_hunt_group_edit.php' alt='add'><img src='".$v_icon_add."' width='17' height='17' border='0' alt='add'></a>\n";
-	echo "</td>\n";
-	echo "<tr>\n";
-
-	if ($resultcount == 0) { //no results
-	}
-	else { //received results
-		foreach($result as $row) {
-			//print_r( $row );
-			echo "<tr >\n";
-			echo "   <td valign='top' class='".$rowstyle[$c]."'>".$row[huntgroupextension]."</td>\n";
-			echo "   <td valign='top' class='".$rowstyle[$c]."'>".$row[huntgroupname]."</td>\n";
-			echo "   <td valign='top' class='".$rowstyle[$c]."'>".$row[hunt_group_enabled]."</td>\n";
-			echo "   <td valign='top' class='rowstylebg' width='40%'>".$row[huntgroupdescr]."&nbsp;</td>\n";
-			echo "   <td valign='top' align='right'>\n";
-			echo "		<a href='v_hunt_group_edit.php?id=".$row[hunt_group_id]."' alt='edit'><img src='".$v_icon_edit."' width='17' height='17' border='0' alt='edit'></a>\n";
-			echo "		<a href='v_hunt_group_delete.php?id=".$row[hunt_group_id]."' alt='delete' onclick=\"return confirm('Do you really want to delete this?')\"><img src='".$v_icon_delete."' width='17' height='17' border='0' alt='delete'></a>\n";
-			echo "   </td>\n";
-			echo "</tr>\n";
-			if ($c==0) { $c=1; } else { $c=0; }
-		} //end foreach
-		unset($sql, $result, $rowcount);
-	} //end if results
-
-	echo "<tr>\n";
-	echo "<td colspan='5'>\n";
-	echo "	<table width='100%' cellpadding='0' cellspacing='0'>\n";
-	echo "	<tr>\n";
-	echo "		<td width='33.3%' nowrap>&nbsp;</td>\n";
-	echo "		<td width='33.3%' align='center' nowrap>$pagingcontrols</td>\n";
-	echo "		<td width='33.3%' align='right'>\n";
-	echo "			<a href='v_hunt_group_edit.php' alt='add'><img src='".$v_icon_add."' width='17' height='17' border='0' alt='add'></a>\n";
-	echo "		</td>\n";
-	echo "	</tr>\n";
-	echo "	</table>\n";
-	echo "</td>\n";
-	echo "</tr>\n";
-
-	if ($v_path_show) {
-		echo "<tr>\n";
-		echo "<td align='left' colspan='4'>\n";
-		echo "<br />\n";
-		echo $v_scripts_dir."\n";
-		echo "</td>\n";
+else { //received results
+	foreach($result as $row) {
+		//print_r( $row );
+		echo "<tr >\n";
+		echo "   <td valign='top' class='".$rowstyle[$c]."'>".$row[huntgroupextension]."</td>\n";
+		echo "   <td valign='top' class='".$rowstyle[$c]."'>".$row[huntgroupname]."</td>\n";
+		echo "   <td valign='top' class='".$rowstyle[$c]."'>".$row[hunt_group_enabled]."</td>\n";
+		echo "   <td valign='top' class='rowstylebg' width='40%'>".$row[huntgroupdescr]."&nbsp;</td>\n";
+		echo "   <td valign='top' align='right'>\n";
+		echo "		<a href='v_hunt_group_edit.php?id=".$row[hunt_group_id]."' alt='edit'><img src='".$v_icon_edit."' width='17' height='17' border='0' alt='edit'></a>\n";
+		echo "		<a href='v_hunt_group_delete.php?id=".$row[hunt_group_id]."' alt='delete' onclick=\"return confirm('Do you really want to delete this?')\"><img src='".$v_icon_delete."' width='17' height='17' border='0' alt='delete'></a>\n";
+		echo "   </td>\n";
 		echo "</tr>\n";
-	}
+		if ($c==0) { $c=1; } else { $c=0; }
+	} //end foreach
+	unset($sql, $result, $rowcount);
+} //end if results
 
-	echo "</table>";
-	echo "</div>";
-	echo "<br><br>";
-	echo "<br><br>";
+echo "<tr>\n";
+echo "<td colspan='5'>\n";
+echo "	<table width='100%' cellpadding='0' cellspacing='0'>\n";
+echo "	<tr>\n";
+echo "		<td width='33.3%' nowrap>&nbsp;</td>\n";
+echo "		<td width='33.3%' align='center' nowrap>$pagingcontrols</td>\n";
+echo "		<td width='33.3%' align='right'>\n";
+echo "			<a href='v_hunt_group_edit.php' alt='add'><img src='".$v_icon_add."' width='17' height='17' border='0' alt='add'></a>\n";
+echo "		</td>\n";
+echo "	</tr>\n";
+echo "	</table>\n";
+echo "</td>\n";
+echo "</tr>\n";
 
-	echo "</td>";
-	echo "</tr>";
-	echo "</table>";
-	echo "</div>";
-	echo "<br><br>";
+if ($v_path_show) {
+	echo "<tr>\n";
+	echo "<td align='left' colspan='4'>\n";
+	echo "<br />\n";
+	echo $v_scripts_dir."\n";
+	echo "</td>\n";
+	echo "</tr>\n";
+}
+
+echo "</table>";
+echo "</div>";
+echo "<br><br>";
+echo "<br><br>";
+
+echo "</td>";
+echo "</tr>";
+echo "</table>";
+echo "</div>";
+echo "<br><br>";
 
 
 require_once "includes/footer.php";
