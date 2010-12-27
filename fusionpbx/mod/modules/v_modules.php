bd53d3dfd286c9145dcd5a8e3135fcbfc6de8a15
Avi Marcus <kLdd16@gmail.com>
2010-12-27 20:47:18 +0000

    Added a cd Documents/VoIP/fusionpbx/ warning to modules page if you set auto start but the module is off.
    git-svn-id: http://fusionpbx.googlecode.com/svn/trunk@645 e2954ef0-caed-11de-bd98-e341d93cc3cc

--- fusionpbx/mod/modules/v_modules.php
+++ fusionpbx/mod/modules/v_modules.php
@@ -217,7 +217,8 @@ if (!function_exists('switch_module_active')) {
 				echo "   <td valign='top' class='".$rowstyle[$c]."'><a href='v_modules.php?a=stop&m=".$row["modulename"]."' alt='stop'>Stop</a></td>\n";
 			}
 			else {
-				echo "   <td valign='top' class='".$rowstyle[$c]."'>Stopped</td>\n";
+				$notice = ($row[moduleenabled]=="true") ? " !!" : null ;
+				echo "   <td valign='top' class='".$rowstyle[$c]."'>Stopped$notice</td>\n";
 				echo "   <td valign='top' class='".$rowstyle[$c]."'><a href='v_modules.php?a=start&m=".$row["modulename"]."' alt='start'>Start</a></td>\n";
 			}
 			echo "   <td valign='top' class='".$rowstyle[$c]."'>".$row["moduleenabled"]."</td>\n";
