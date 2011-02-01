c5853d4b26d2d9b661797e51f1788078e16eee4a
Mark J Crane <markjcrane@fusionpbx.com>
2011-02-01 01:29:44 +0000

    Apps -> Call Center set the user_status when the status is changed.
    git-svn-id: http://fusionpbx.googlecode.com/svn/trunk@789 e2954ef0-caed-11de-bd98-e341d93cc3cc

--- fusionpbx/mod/call_center/v_call_center_agent_edit.php
+++ fusionpbx/mod/call_center/v_call_center_agent_edit.php
@@ -88,6 +88,14 @@ if (count($_POST)>0 && strlen($_POST["persistformvar"]) == 0) {
 			return;
 		}
 
+	//set the user_status
+		$sql  = "update v_users set ";
+		$sql .= "user_status = '".$agent_status."' ";
+		$sql .= "where v_id = '$v_id' ";
+		$sql .= "and username = '".$agent_name."' ";
+ 		$prepstatement = $db->prepare(check_sql($sql));
+		$prepstatement->execute();
+
 	//add or update the database
 	if ($_POST["persistformvar"] != "true") {
 		if ($action == "add") {
