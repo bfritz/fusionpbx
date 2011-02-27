b769f2f4c854fbbcaca8b7f6b496a2b503d27b4e
Mark J Crane <markjcrane@fusionpbx.com>
2011-02-27 00:50:01 +0000

    Remove the running total registration count that I used for testing.
    git-svn-id: http://fusionpbx.googlecode.com/svn/trunk@852 e2954ef0-caed-11de-bd98-e341d93cc3cc

--- fusionpbx/core/status/v_status_registrations.php
+++ fusionpbx/core/status/v_status_registrations.php
@@ -118,7 +118,7 @@ require_once "includes/checkauth.php";
 						//<td class='".$rowstyle[$c]."'>&nbsp;".$row->{'call-id'}."&nbsp;</td>\n";
 						//echo "	<td class='".$rowstyle[$c]."'>&nbsp;".$row->{'user'}."&nbsp;</td>\n";
 						//echo "	<td class='".$rowstyle[$c]."'>&nbsp;".$row->{'contact'}."&nbsp;</td>\n";
-						echo "	<td class='".$rowstyle[$c]."'>$registration_count &nbsp;".$row->{'sip-auth-user'}."&nbsp;</td>\n";
+						echo "	<td class='".$rowstyle[$c]."'>&nbsp;".$row->{'sip-auth-user'}."&nbsp;</td>\n";
 						echo "	<td class='".$rowstyle[$c]."'>&nbsp;".$row->{'agent'}."&nbsp;</td>\n";
 						//echo "	<td class='".$rowstyle[$c]."'>&nbsp;".$row->{'host'}."&nbsp;</td>\n";
 						echo "	<td class='".$rowstyle[$c]."'>&nbsp;<a href='http://".$row->{'network-ip'}."' target='_blank'>".$row->{'network-ip'}."</a>&nbsp;</td>\n";
