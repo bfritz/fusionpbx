2fef8d56cc0ce0416e5721b31e3f0bb4954dea4b
Mark J Crane <markjcrane@fusionpbx.com>
2011-03-12 18:18:08 +0000

    Add send_http_request function.
    git-svn-id: http://fusionpbx.googlecode.com/svn/trunk@914 e2954ef0-caed-11de-bd98-e341d93cc3cc

--- fusionpbx/includes/lib_functions.php
+++ fusionpbx/includes/lib_functions.php
@@ -736,4 +736,27 @@ function format_string ($format, $data) {
 		);
 	} 
 
+//based on Wez Furlong do_post_request
+	if (!function_exists('send_http_request')) {
+		function send_http_request($url, $data, $method = "POST", $optional_headers = null) {
+			$params = array('http' => array(
+						'method' => $method,
+						'content' => $data
+						));
+			if ($optional_headers !== null) {
+				$params['http']['header'] = $optional_headers;
+			}
+			$ctx = stream_context_create($params);
+			$fp = @fopen($url, 'rb', false, $ctx);
+			if (!$fp) {
+				throw new Exception("Problem with $url, $php_errormsg");
+			}
+			$response = @stream_get_contents($fp);
+			if ($response === false) {
+				throw new Exception("Problem reading data from $url, $php_errormsg");
+			}
+			return $response;
+		}
+	}
+
 ?>
