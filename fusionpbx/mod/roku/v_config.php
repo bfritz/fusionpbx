f645c338a554964acfbd2c5d0a6dd8f3c018dd6f
Mark J Crane <markjcrane@fusionpbx.com>
2011-04-06 11:55:26 +0000

    Define the groups that will have access by default to the permissions and re-organize the v_config.php file.
    git-svn-id: http://fusionpbx.googlecode.com/svn/trunk@977 e2954ef0-caed-11de-bd98-e341d93cc3cc

--- fusionpbx/mod/roku/v_config.php
+++ fusionpbx/mod/roku/v_config.php
@@ -1,22 +1,26 @@
 <?php
 	/*
-	$apps[$x]['name'] = "Roku";
-	$apps[$x]['guid'] = 'zzz';
-	$apps[$x]['category'] = '';
-	$apps[$x]['subcategory'] = '';
-	$apps[$x]['version'] = '';
-	$apps[$x]['menu'][0]['title']['en'] = 'Roku';
-	$apps[$x]['menu'][0]['guid'] = 'zzz';
-	$apps[$x]['menu'][0]['parent_guid'] = 'zzz';
-	$apps[$x]['menu'][0]['category'] = 'internal';
-	$apps[$x]['menu'][0]['path'] = '/mod/zzz/zzz.php';
-	//$apps[$x]['menu'][0]['groups'][] = 'superadmin';
-	$apps[$x]['permissions'][] = '_view';
-	$apps[$x]['permissions'][] = '_add';
-	$apps[$x]['permissions'][] = '_edit';
-	$apps[$x]['permissions'][] = '_delete';
-	$apps[$x]['license'] = 'Mozilla Public License 1.1';
-	$apps[$x]['url'] = 'http://www.fusionpbx.com';
-	$apps[$x]['description']['en'] = 'zzz.';
+	//application details
+		$apps[$x]['name'] = "Roku";
+		$apps[$x]['guid'] = 'zzz';
+		$apps[$x]['category'] = '';
+		$apps[$x]['subcategory'] = '';
+		$apps[$x]['version'] = '';
+		$apps[$x]['license'] = 'Mozilla Public License 1.1';
+		$apps[$x]['url'] = 'http://www.fusionpbx.com';
+		$apps[$x]['description']['en'] = 'zzz.';
+
+	//menu details
+		$apps[$x]['menu'][0]['title']['en'] = 'Roku';
+		$apps[$x]['menu'][0]['guid'] = 'zzz';
+		$apps[$x]['menu'][0]['parent_guid'] = 'zzz';
+		$apps[$x]['menu'][0]['category'] = 'internal';
+		$apps[$x]['menu'][0]['path'] = '/mod/zzz/zzz.php';
+		//$apps[$x]['menu'][0]['groups'][] = 'superadmin';
+
+	//permission details
+		$apps[$x]['permissions'][0]['name'] = 'zzz';
+		$apps[$x]['permissions'][0]['groups'][] = 'admin';
+		$apps[$x]['permissions'][0]['groups'][] = 'superadmin';
 	*/
 ?>
