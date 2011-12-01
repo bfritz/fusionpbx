39106a57286d04e470c472fbdda7f60070852a27
Mark J Crane <markjcrane@fusionpbx.com>
2011-12-01 10:29:43 +0000

    xml cdr conf xml file default it to http://127.0.0.1
    git-svn-id: http://fusionpbx.googlecode.com/svn/trunk@1716 e2954ef0-caed-11de-bd98-e341d93cc3cc

--- fusionpbx/includes/lib_switch.php
+++ fusionpbx/includes/lib_switch.php
@@ -6009,14 +6009,8 @@ if (!function_exists('xml_cdr_conf_xml')) {
 			$file_contents = file_get_contents($_SERVER["DOCUMENT_ROOT"].PROJECT_PATH."/includes/templates/conf/autoload_configs/xml_cdr.conf.xml");
 
 		//replace the values in the template
-			if ($_SERVER["HTTPS"] == "on") {
-				$file_contents = str_replace("{v_http_protocol}", "https", $file_contents);
-			}
-			else {
-				$file_contents = str_replace("{v_http_protocol}", "http", $file_contents);
-			}
-
-			$file_contents = str_replace("{v_domain}", $v_domain, $file_contents);
+			$file_contents = str_replace("{v_http_protocol}", "http", $file_contents);
+			$file_contents = str_replace("{v_domain}", "127.0.0.1", $file_contents);
 			$file_contents = str_replace("{v_project_path}", PROJECT_PATH, $file_contents);
 
 			$v_user = generate_password();
