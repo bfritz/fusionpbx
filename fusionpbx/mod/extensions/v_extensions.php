e213996d59da51cb1ec092680e4d3f4ad0b4ed9f
Mark J Crane <markjcrane@fusionpbx.com>
2011-12-01 01:44:44 +0000

    Improve the tab spacing and the comments for extensions.
    git-svn-id: http://fusionpbx.googlecode.com/svn/trunk@1712 e2954ef0-caed-11de-bd98-e341d93cc3cc

--- fusionpbx/mod/extensions/v_extensions.php
+++ fusionpbx/mod/extensions/v_extensions.php
@@ -47,14 +47,15 @@ require_once "includes/paging.php";
 	echo "	<td align=\"center\">\n";
 	echo "      <br>";
 
-	echo "<table width=\"100%\" border=\"0\" cellpadding=\"6\" cellspacing=\"0\">\n";
-	echo "  <tr>\n";
-	echo "	<td align='left'><b>Extensions</b><br>\n";
-	echo "		Use this to configure your SIP extensions.\n";
-	echo "	</td>\n";
-	echo "  </tr>\n";
-	echo "</table>\n";
-	echo "<br />";
+	//show the content header
+		echo "<table width=\"100%\" border=\"0\" cellpadding=\"6\" cellspacing=\"0\">\n";
+		echo "  <tr>\n";
+		echo "	<td align='left'><b>Extensions</b><br>\n";
+		echo "		Use this to configure your SIP extensions.\n";
+		echo "	</td>\n";
+		echo "  </tr>\n";
+		echo "</table>\n";
+		echo "<br />";
 
 	//get the number of rows in v_extensions 
 		$sql = "";
@@ -95,81 +96,79 @@ require_once "includes/paging.php";
 		$prepstatement = $db->prepare(check_sql($sql));
 		$prepstatement->execute();
 		$result = $prepstatement->fetchAll();
-		$resultcount = count($result);
+		$result_count = count($result);
 		unset ($prepstatement, $sql);
 
-	$c = 0;
-	$rowstyle["0"] = "rowstyle0";
-	$rowstyle["1"] = "rowstyle1";
+		$c = 0;
+		$rowstyle["0"] = "rowstyle0";
+		$rowstyle["1"] = "rowstyle1";
 
-	echo "<div align='center'>\n";
-	echo "<table width='100%' border='0' cellpadding='0' cellspacing='0'>\n";
-	echo "<tr>\n";
-	echo thorderby('extension', 'Extension', $orderby, $order);
-	echo thorderby('callgroup', 'Call Group', $orderby, $order);
-	echo thorderby('vm_mailto', 'Voicemail Mail To', $orderby, $order);
-	echo thorderby('enabled', 'Enabled', $orderby, $order);
-	echo thorderby('description', 'Description', $orderby, $order);
-	echo "<td align='right' width='42'>\n";
-	if (permission_exists('extension_add')) {
-		echo "	<a href='v_extensions_edit.php' alt='add'>$v_link_label_add</a>\n";
-	}
-	echo "</td>\n";
-	echo "<tr>\n";
+		echo "<div align='center'>\n";
+		echo "<table width='100%' border='0' cellpadding='0' cellspacing='0'>\n";
+		echo "<tr>\n";
+		echo thorderby('extension', 'Extension', $orderby, $order);
+		echo thorderby('callgroup', 'Call Group', $orderby, $order);
+		echo thorderby('vm_mailto', 'Voicemail Mail To', $orderby, $order);
+		echo thorderby('enabled', 'Enabled', $orderby, $order);
+		echo thorderby('description', 'Description', $orderby, $order);
+		echo "<td align='right' width='42'>\n";
+		if (permission_exists('extension_add')) {
+			echo "	<a href='v_extensions_edit.php' alt='add'>$v_link_label_add</a>\n";
+		}
+		echo "</td>\n";
+		echo "<tr>\n";
 
-	if ($resultcount == 0) { //no results
-	}
-	else { //received results
-		foreach($result as $row) {
-			echo "<tr >\n";
-			echo "	<td valign='top' class='".$rowstyle[$c]."'>".$row['extension']."</td>\n";
-			echo "	<td valign='top' class='".$rowstyle[$c]."'>".$row['callgroup']."&nbsp;</td>\n";
-			echo "	<td valign='top' class='".$rowstyle[$c]."'>".$row['vm_mailto']."&nbsp;</td>\n";
-			echo "	<td valign='top' class='".$rowstyle[$c]."'>".$row['enabled']."</td>\n";
-			echo "	<td valign='top' class='rowstylebg' width='30%'>".$row['description']."&nbsp;</td>\n";
-			echo "	<td valign='top' align='right'>\n";
-			if (permission_exists('extension_edit')) {
-				echo "		<a href='v_extensions_edit.php?id=".$row['extension_id']."' alt='edit'>$v_link_label_edit</a>\n";
-			}
-			if (permission_exists('extension_delete')) {
-				echo "		<a href='v_extensions_delete.php?id=".$row['extension_id']."' alt='delete' onclick=\"return confirm('Do you really want to delete this?')\">$v_link_label_delete</a>\n";
-			}
-			echo "	</td>\n";
-			echo "</tr>\n";
-			if ($c==0) { $c=1; } else { $c=0; }
-		} //end foreach
-		unset($sql, $result, $rowcount);
-	} //end if results
+		if ($result_count > 0) {
+			foreach($result as $row) {
+				echo "<tr >\n";
+				echo "	<td valign='top' class='".$rowstyle[$c]."'>".$row['extension']."</td>\n";
+				echo "	<td valign='top' class='".$rowstyle[$c]."'>".$row['callgroup']."&nbsp;</td>\n";
+				echo "	<td valign='top' class='".$rowstyle[$c]."'>".$row['vm_mailto']."&nbsp;</td>\n";
+				echo "	<td valign='top' class='".$rowstyle[$c]."'>".$row['enabled']."</td>\n";
+				echo "	<td valign='top' class='rowstylebg' width='30%'>".$row['description']."&nbsp;</td>\n";
+				echo "	<td valign='top' align='right'>\n";
+				if (permission_exists('extension_edit')) {
+					echo "		<a href='v_extensions_edit.php?id=".$row['extension_id']."' alt='edit'>$v_link_label_edit</a>\n";
+				}
+				if (permission_exists('extension_delete')) {
+					echo "		<a href='v_extensions_delete.php?id=".$row['extension_id']."' alt='delete' onclick=\"return confirm('Do you really want to delete this?')\">$v_link_label_delete</a>\n";
+				}
+				echo "	</td>\n";
+				echo "</tr>\n";
+				if ($c==0) { $c=1; } else { $c=0; }
+			} //end foreach
+			unset($sql, $result, $rowcount);
+		} //end if results
 
-	echo "<tr>\n";
-	echo "<td colspan='6' align='left'>\n";
-	echo "	<table border='0' width='100%' cellpadding='0' cellspacing='0'>\n";
-	echo "	<tr>\n";
-	echo "		<td width='33.3%' nowrap>&nbsp;</td>\n";
-	echo "		<td width='33.3%' align='center' nowrap>$pagingcontrols</td>\n";
-	echo "		<td width='33.3%' align='right'>\n";
-	if (permission_exists('extension_add')) {
-		echo "			<a href='v_extensions_edit.php' alt='add'>$v_link_label_add</a>\n";
-	}
-	echo "		</td>\n";
-	echo "	</tr>\n";
-	echo "	</table>\n";
-	echo "</td>\n";
-	echo "</tr>\n";
+		echo "<tr>\n";
+		echo "<td colspan='6' align='left'>\n";
+		echo "	<table border='0' width='100%' cellpadding='0' cellspacing='0'>\n";
+		echo "	<tr>\n";
+		echo "		<td width='33.3%' nowrap>&nbsp;</td>\n";
+		echo "		<td width='33.3%' align='center' nowrap>$pagingcontrols</td>\n";
+		echo "		<td width='33.3%' align='right'>\n";
+		if (permission_exists('extension_add')) {
+			echo "			<a href='v_extensions_edit.php' alt='add'>$v_link_label_add</a>\n";
+		}
+		echo "		</td>\n";
+		echo "	</tr>\n";
+		echo "	</table>\n";
+		echo "</td>\n";
+		echo "</tr>\n";
 
-	echo "<tr>\n";
-	echo "<td colspan='5' align='left'>\n";
-	echo "<br />\n";
-	if ($v_path_show) {
-		echo $v_extensions_dir."\n";
-	}
-	echo "</td>\n";
-	echo "</tr>\n";
+		echo "<tr>\n";
+		echo "<td colspan='5' align='left'>\n";
+		echo "<br />\n";
+		if ($v_path_show) {
+			echo $v_extensions_dir."\n";
+		}
+		echo "</td>\n";
+		echo "</tr>\n";
 
-	echo "</table>";
-	echo "</div>";
-	echo "<br><br>";
-	echo "<br><br>";
+		echo "</table>";
+		echo "</div>";
+		echo "<br><br>";
+		echo "<br><br>";
 
 	echo "</td>";
 	echo "</tr>";
