2f07cdc16425e2af8c9e7e09c52a8d41fb4f1fc2
Mark J Crane <markjcrane@fusionpbx.com>
2012-01-19 07:15:21 +0000

    On new installs setup permissions to allow admins to see and use the contacts
    git-svn-id: http://fusionpbx.googlecode.com/svn/trunk@1884 e2954ef0-caed-11de-bd98-e341d93cc3cc

--- fusionpbx/mod/contacts/v_config.php
+++ fusionpbx/mod/contacts/v_config.php
@@ -16,27 +16,27 @@
 		$apps[$x]['menu'][$y]['category'] = 'internal';
 		$apps[$x]['menu'][$y]['path'] = '/mod/contacts/v_contacts.php';
 		//$apps[$x]['menu'][$y]['groups'][] = 'user';
-		//$apps[$x]['menu'][$y]['groups'][] = 'admin';
+		$apps[$x]['menu'][$y]['groups'][] = 'admin';
 		$apps[$x]['menu'][$y]['groups'][] = 'superadmin';
 
 	//permission details
-		$apps[$x]['permissions'][$y]['name'] = 'contacts_view';
-		$apps[$x]['permissions'][$y]['groups'][] = 'superadmin';
-		//$apps[$x]['permissions'][$y]['groups'][] = 'user';
-		//$apps[$x]['permissions'][$y]['groups'][] = 'admin';
+		$apps[$x]['permissions'][0]['name'] = 'contacts_view';
+		$apps[$x]['permissions'][0]['groups'][] = 'superadmin';
+		//$apps[$x]['permissions'][0]['groups'][] = 'user';
+		$apps[$x]['permissions'][0]['groups'][] = 'admin';
 
 		$apps[$x]['permissions'][1]['name'] = 'contacts_add';
 		$apps[$x]['permissions'][1]['groups'][] = 'superadmin';
-		//$apps[$x]['permissions'][1]['groups'][] = 'admin';
+		$apps[$x]['permissions'][1]['groups'][] = 'admin';
 
 		$apps[$x]['permissions'][2]['name'] = 'contacts_edit';
 		$apps[$x]['permissions'][2]['groups'][] = 'superadmin';
-		//$apps[$x]['permissions'][2]['groups'][] = 'admin';
+		$apps[$x]['permissions'][2]['groups'][] = 'admin';
 		//$apps[$x]['permissions'][2]['groups'][] = 'user';
 
 		$apps[$x]['permissions'][3]['name'] = 'contacts_delete';
 		$apps[$x]['permissions'][3]['groups'][] = 'superadmin';
-		//$apps[$x]['permissions'][3]['groups'][] = 'admin';
+		$apps[$x]['permissions'][3]['groups'][] = 'admin';
 
 	//schema details
 		$y = 0; //table array index
