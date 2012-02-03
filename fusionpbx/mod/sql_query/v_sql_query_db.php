45ec77153b83e993e8375a077c54b89c626be380
Mark J Crane <markjcrane@fusionpbx.com>
2012-02-03 10:19:07 +0000

    Change the database and fields the sql query database select tool uses.
    git-svn-id: http://fusionpbx.googlecode.com/svn/trunk@1948 e2954ef0-caed-11de-bd98-e341d93cc3cc

--- fusionpbx/mod/sql_query/v_sql_query_db.php
+++ fusionpbx/mod/sql_query/v_sql_query_db.php
@@ -49,19 +49,19 @@ require_once "includes/paging.php";
 
 	echo "<table width='100%' border='0'>\n";
 	echo "	<tr>\n";
-	echo "		<td width='50%' align=\"left\" nowrap=\"nowrap\"><b>Database Connections</b></td>\n";
+	echo "		<td width='50%' align=\"left\" nowrap=\"nowrap\"><b>Databases</b></td>\n";
 	echo "		<td width='50%' align=\"right\">&nbsp;</td>\n";
 	echo "	</tr>\n";
 	echo "	<tr>\n";
 	echo "		<td align=\"left\" colspan='2'>\n";
-	echo "			Select the database connection to use.<br /><br />\n";
+	echo "			Select the database to use.<br /><br />\n";
 	echo "		</td>\n";
 	echo "	</tr>\n";
 	echo "</table>\n";
 
 	//prepare to page the results
 		$sql = "";
-		$sql .= " select count(*) as num_rows from v_db ";
+		$sql .= " select count(*) as num_rows from v_databases ";
 		if (strlen($order_by)> 0) { $sql .= "order by $order_by $order "; }
 		$prep_statement = $db->prepare($sql);
 		if ($prep_statement) {
@@ -76,7 +76,7 @@ require_once "includes/paging.php";
 		}
 
 	//prepare to page the results
-		$rows_per_page = 10;
+		$rows_per_page = 100;
 		$param = "";
 		$page = $_GET['page'];
 		if (strlen($page) == 0) { $page = 0; $_GET['page'] = 0; } 
@@ -85,7 +85,7 @@ require_once "includes/paging.php";
 
 	//get the  list
 		$sql = "";
-		$sql .= " select * from v_db ";
+		$sql .= " select * from v_databases ";
 		if (strlen($order_by)> 0) { $sql .= "order by $order_by $order "; }
 		$sql .= " limit $rows_per_page offset $offset ";
 		$prep_statement = $db->prepare(check_sql($sql));
@@ -102,14 +102,13 @@ require_once "includes/paging.php";
 	echo "<table width='100%' border='0' cellpadding='0' cellspacing='0'>\n";
 
 	echo "<tr>\n";
-	echo thorder_by('db_type', 'Type', $order_by, $order);
-	echo thorder_by('db_host', 'Host', $order_by, $order);
-	//echo thorder_by('db_port', 'Port', $order_by, $order);
-	echo thorder_by('db_name', 'Name', $order_by, $order);
-	//echo thorder_by('db_username', 'Username', $order_by, $order);
-	//echo thorder_by('db_password', 'Password', $order_by, $order);
-	//echo thorder_by('db_path', 'Path', $order_by, $order);
-	echo thorder_by('db_description', 'Description', $order_by, $order);
+	echo thorder_by('database_type', 'Type', $order_by, $order);
+	echo thorder_by('database_host', 'Host', $order_by, $order);
+	//echo thorder_by('database_port', 'Port', $order_by, $order);
+	echo thorder_by('database_name', 'Name', $order_by, $order);
+	//echo thorder_by('database_username', 'Username', $order_by, $order);
+	//echo thorder_by('database_path', 'Path', $order_by, $order);
+	echo thorder_by('database_description', 'Description', $order_by, $order);
 	echo "<td align='right' width='21'>\n";
 	//echo "	<a href='db_edit.php' alt='add'>$v_link_label_add</a>\n";
 	echo "</td>\n";
@@ -117,16 +116,14 @@ require_once "includes/paging.php";
 
 	if ($result_count > 0) {
 		foreach($result as $row) {
-			//print_r( $row );
 			echo "<tr >\n";
-			echo "	<td valign='top' class='".$row_style[$c]."'>".$row['db_type']."&nbsp;</td>\n";
-			echo "	<td valign='top' class='".$row_style[$c]."'>".$row['db_host']."&nbsp;</td>\n";
-			//echo "	<td valign='top' class='".$row_style[$c]."'>".$row['db_port']."&nbsp;</td>\n";
-			echo "	<td valign='top' class='".$row_style[$c]."'>".$row['db_name']."&nbsp;</td>\n";
-			//echo "	<td valign='top' class='".$row_style[$c]."'>".$row['db_username']."&nbsp;</td>\n";
-			//echo "	<td valign='top' class='".$row_style[$c]."'>".$row['db_password']."&nbsp;</td>\n";
-			//echo "	<td valign='top' class='".$row_style[$c]."'>".$row['db_path']."&nbsp;</td>\n";
-			echo "	<td valign='top' class='row_stylebg'>".$row['db_description']."&nbsp;</td>\n";
+			echo "	<td valign='top' class='".$row_style[$c]."'>".$row['database_type']."&nbsp;</td>\n";
+			echo "	<td valign='top' class='".$row_style[$c]."'>".$row['database_host']."&nbsp;</td>\n";
+			//echo "	<td valign='top' class='".$row_style[$c]."'>".$row['database_port']."&nbsp;</td>\n";
+			echo "	<td valign='top' class='".$row_style[$c]."'>".$row['database_name']."&nbsp;</td>\n";
+			//echo "	<td valign='top' class='".$row_style[$c]."'>".$row['database_username']."&nbsp;</td>\n";
+			//echo "	<td valign='top' class='".$row_style[$c]."'>".$row['database_path']."&nbsp;</td>\n";
+			echo "	<td valign='top' class='row_stylebg'>".$row['database_description']."&nbsp;</td>\n";
 			echo "	<td valign='top' align='right'>\n";
 			echo "		<a href='v_sql_query.php?id=".$row['db_uuid']."' alt='edit'>$v_link_label_edit</a>\n";
 			echo "	</td>\n";
