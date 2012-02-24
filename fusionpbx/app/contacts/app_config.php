0d12be6c7543448864e407747dde180e9bb966ec
Mark J Crane <markjcrane@fusionpbx.com>
2012-02-24 21:44:28 +0000

    Fix a problem with the contacts menu.
    git-svn-id: http://fusionpbx.googlecode.com/svn/trunk@2183 e2954ef0-caed-11de-bd98-e341d93cc3cc

--- fusionpbx/app/contacts/app_config.php
+++ fusionpbx/app/contacts/app_config.php
@@ -10,7 +10,7 @@
 		$apps[$x]['description']['en'] = '';
 
 	//menu details
-		$apps[$x]['menu'][$y]['contact_title']['en'] = 'Contacts';
+		$apps[$x]['menu'][$y]['title']['en'] = 'Contacts';
 		$apps[$x]['menu'][$y]['uuid'] = 'f14e6ab6-6565-d4e6-cbad-a51d2e3e8ec6';
 		$apps[$x]['menu'][$y]['parent_uuid'] = 'fd29e39c-c936-f5fc-8e2b-611681b266b5';
 		$apps[$x]['menu'][$y]['category'] = 'internal';
