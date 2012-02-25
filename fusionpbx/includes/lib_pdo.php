6bcfdf53bd916bb28c7a2302e94cf5c3b3b7b65a
Mark J Crane <markjcrane@fusionpbx.com>
2012-02-25 07:09:58 +0000

    When deleting a domain remove the data and files for that domain and set the session values only when the menu session variable hasn't been set.
    git-svn-id: http://fusionpbx.googlecode.com/svn/trunk@2195 e2954ef0-caed-11de-bd98-e341d93cc3cc

--- fusionpbx/includes/lib_pdo.php
+++ fusionpbx/includes/lib_pdo.php
@@ -342,63 +342,66 @@ if ($db_type == "pgsql") {
 			unset($result, $prep_statement);
 	}
 
-//get the default settings
-	$sql = "select * from v_default_settings ";
-	$sql .= "where default_setting_enabled = 'true' ";
-	$prep_statement = $db->prepare($sql);
-	$prep_statement->execute();
-	$result = $prep_statement->fetchAll();
-	foreach($result as $row) {
-		$name = $row['default_setting_name'];
-		$category = $row['default_setting_category'];
-		$subcategory = $row['default_setting_subcategory'];	
-		if (strlen($subcategory) == 0) {
-			$_SESSION[$category][$name] = $row['default_setting_value'];
-		}
-		else {
-			$_SESSION[$category][$subcategory][$name] = $row['default_setting_value'];
-		}
-	}
+//get the session settings
+	if (strlen($_SESSION['domain']['menu']) == 0) {
+		//get the default settings
+			$sql = "select * from v_default_settings ";
+			$sql .= "where default_setting_enabled = 'true' ";
+			$prep_statement = $db->prepare($sql);
+			$prep_statement->execute();
+			$result = $prep_statement->fetchAll();
+			foreach($result as $row) {
+				$name = $row['default_setting_name'];
+				$category = $row['default_setting_category'];
+				$subcategory = $row['default_setting_subcategory'];	
+				if (strlen($subcategory) == 0) {
+					$_SESSION[$category][$name] = $row['default_setting_value'];
+				}
+				else {
+					$_SESSION[$category][$subcategory][$name] = $row['default_setting_value'];
+				}
+			}
 
-//get the domains settings
-	$sql = "select * from v_domain_settings ";
-	$sql .= "where domain_uuid = '".$_SESSION["domain_uuid"]."' ";
-	$sql .= "and domain_setting_enabled = 'true' ";
-	$prep_statement = $db->prepare($sql);
-	$prep_statement->execute();
-	$result = $prep_statement->fetchAll();
-	foreach($result as $row) {
-		$name = $row['domain_setting_name'];
-		$category = $row['domain_setting_category'];
-		$subcategory = $row['domain_setting_subcategory'];	
-		if (strlen($subcategory) == 0) {
-			//$$category[$name] = $row['domain_setting_value'];
-			$_SESSION[$category][$name] = $row['domain_setting_value'];
-		}
-		else {
-			//$$category[$subcategory][$name] = $row['domain_setting_value'];
-			$_SESSION[$category][$subcategory][$name] = $row['domain_setting_value'];
-		}
-	}
+		//get the domains settings
+			$sql = "select * from v_domain_settings ";
+			$sql .= "where domain_uuid = '".$_SESSION["domain_uuid"]."' ";
+			$sql .= "and domain_setting_enabled = 'true' ";
+			$prep_statement = $db->prepare($sql);
+			$prep_statement->execute();
+			$result = $prep_statement->fetchAll();
+			foreach($result as $row) {
+				$name = $row['domain_setting_name'];
+				$category = $row['domain_setting_category'];
+				$subcategory = $row['domain_setting_subcategory'];	
+				if (strlen($subcategory) == 0) {
+					//$$category[$name] = $row['domain_setting_value'];
+					$_SESSION[$category][$name] = $row['domain_setting_value'];
+				}
+				else {
+					//$$category[$subcategory][$name] = $row['domain_setting_value'];
+					$_SESSION[$category][$subcategory][$name] = $row['domain_setting_value'];
+				}
+			}
 
-//set the values from the session variables
-	if (strlen($_SESSION['domain']['time_zone']['name']) > 0) {
-		//server time zone
-			$_SESSION['time_zone']['system'] = date_default_timezone_get();
-		//domain time zone set in system settings
-			$_SESSION['time_zone']['domain'] = $_SESSION['domain']['time_zone']['name'];
-		//set the domain time zone as the default time zone
-			date_default_timezone_set($_SESSION['domain']['time_zone']['name']);
-	}
+		//set the values from the session variables
+			if (strlen($_SESSION['domain']['time_zone']['name']) > 0) {
+				//server time zone
+					$_SESSION['time_zone']['system'] = date_default_timezone_get();
+				//domain time zone set in system settings
+					$_SESSION['time_zone']['domain'] = $_SESSION['domain']['time_zone']['name'];
+				//set the domain time zone as the default time zone
+					date_default_timezone_set($_SESSION['domain']['time_zone']['name']);
+			}
 
-//set the context
-	if (strlen($_SESSION["context"]) == 0) {
-		if (count($_SESSION["domains"]) > 1) {
-			$_SESSION["context"] = $_SESSION["domain_name"];
-		}
-		else {
-			$_SESSION["context"] = 'default';
-		}
+		//set the context
+			if (strlen($_SESSION["context"]) == 0) {
+				if (count($_SESSION["domains"]) > 1) {
+					$_SESSION["context"] = $_SESSION["domain_name"];
+				}
+				else {
+					$_SESSION["context"] = 'default';
+				}
+			}
 	}
 
 //set the domain_uuid variable from the session
