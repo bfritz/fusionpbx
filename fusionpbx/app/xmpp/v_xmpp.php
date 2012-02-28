cfb381b0dab237573646f255af9df00b7281f0db
Mark J Crane <markjcrane@fusionpbx.com>
2012-02-28 08:05:43 +0000

    Remove some redudant code.
    git-svn-id: http://fusionpbx.googlecode.com/svn/trunk@2210 e2954ef0-caed-11de-bd98-e341d93cc3cc

--- fusionpbx/app/xmpp/v_xmpp.php
+++ fusionpbx/app/xmpp/v_xmpp.php
@@ -38,25 +38,6 @@ else {
 require_once "includes/header.php";
 require_once "includes/paging.php";
 
-if ($_SESSION['db_tables']['v_xmpp'] != 'valid') {
-	if ($db_type == "pgsql") {
-		$sql = "select count(*) from pg_tables where schemaname='public' and table_name = 'v_xmpp';";
-	} elseif ($db_type == "mysql") {
-		$sql = "select count(*) as count from information_schema.tables where TABLE_SCHEMA='" . $db_name . "' and TABLE_NAME='roomlist';";
-	} elseif ($db_type == "sqlite") {
-		$sql = "select count(*) as count from sqlite_master WHERE type IN ('table','view') AND name = 'registrations';";
-	}
-
-	$row = $db->query($sql)->fetch();
-
-	if ($row['count'] < 1) {
-		include "db_create.php";
-		$db->exec(sql_tables($db_type));
-		// $create = $db->query(sql_tables($db_type))->fetch();
-		$_SESSION['db_tables']['v_xmpp'] = 'valid';
-	}
-}
-
 //get a list of assigned extensions for this user
 $sql = "";
 $sql .= "select * from v_xmpp ";
