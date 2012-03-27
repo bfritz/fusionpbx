ebd8e7c5d6d6c7aab1f296da96210e024e2150ba
Mark J Crane <markjcrane@fusionpbx.com>
2012-03-27 16:57:39 +0000

    Uncomment group_uuid to fix the install.
    git-svn-id: http://fusionpbx.googlecode.com/svn/trunk@2341 e2954ef0-caed-11de-bd98-e341d93cc3cc

--- fusionpbx/core/users/app_config.php
+++ fusionpbx/core/users/app_config.php
@@ -189,13 +189,13 @@
 		$y = 2; //table array index
 		$z = 0; //field array index
 		$apps[$x]['db'][$y]['table'] = 'v_groups';
-//		$apps[$x]['db'][$y]['fields'][$z]['name'] = 'group_uuid';
-//		$apps[$x]['db'][$y]['fields'][$z]['type']['pgsql'] = 'uuid';
-//		$apps[$x]['db'][$y]['fields'][$z]['type']['sqlite'] = 'text';
-//		$apps[$x]['db'][$y]['fields'][$z]['type']['mysql'] = 'char(36)';
-//		$apps[$x]['db'][$y]['fields'][$z]['key']['type'] = 'primary';
-//		$apps[$x]['db'][$y]['fields'][$z]['description']['en'] = '';
-//		$z++;
+		$apps[$x]['db'][$y]['fields'][$z]['name'] = 'group_uuid';
+		$apps[$x]['db'][$y]['fields'][$z]['type']['pgsql'] = 'uuid';
+		$apps[$x]['db'][$y]['fields'][$z]['type']['sqlite'] = 'text';
+		$apps[$x]['db'][$y]['fields'][$z]['type']['mysql'] = 'char(36)';
+		$apps[$x]['db'][$y]['fields'][$z]['key']['type'] = 'primary';
+		$apps[$x]['db'][$y]['fields'][$z]['description']['en'] = '';
+		$z++;
 		$apps[$x]['db'][$y]['fields'][$z]['name'] = 'id';
 		$apps[$x]['db'][$y]['fields'][$z]['type']['pgsql'] = 'serial';
 		$apps[$x]['db'][$y]['fields'][$z]['type']['sqlite'] = 'integer';
