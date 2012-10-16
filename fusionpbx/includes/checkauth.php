4c444266a9ea3fc7ce1440e88326defdd2e075a3
Mark J Crane <markjcrane@fusionpbx.com>
2012-10-16 18:14:09 +0000

    Add the reference to the domains class when logging in fixes session variables needed when logging in with username@domain (allowing to login to a different domain).
    git-svn-id: http://fusionpbx.googlecode.com/svn/trunk@3043 e2954ef0-caed-11de-bd98-e341d93cc3cc

--- fusionpbx/includes/checkauth.php
+++ fusionpbx/includes/checkauth.php
@@ -56,11 +56,19 @@ session_start();
 			if (count($username_array) > 1 || strlen(check_str($_REQUEST["domain_name"])) > 0) {
 				foreach ($_SESSION['domains'] as &$row) {
 					if ($row['domain_name'] == $domain_name) {
-						$domain_uuid = $row["domain_uuid"];
-						$_SESSION["domain_uuid"] = $row["domain_uuid"];
-						$_SESSION['domains'][$row['domain_uuid']]['domain_uuid'] = $row['domain_uuid'];
-						$_SESSION['domains'][$row['domain_uuid']]['domain_name'] = $domain_name;
-						$_SESSION["domain_name"] = $domain_name;
+						//set the domain session variables
+							$domain_uuid = $row["domain_uuid"];
+							$_SESSION["domain_uuid"] = $row["domain_uuid"];
+							$_SESSION['domains'][$row['domain_uuid']]['domain_uuid'] = $row['domain_uuid'];
+							$_SESSION['domains'][$row['domain_uuid']]['domain_name'] = $domain_name;
+							$_SESSION["domain_name"] = $domain_name;
+
+						//set the setting arrays
+							//domains set()
+							require "includes/classes/domains.php";
+							$domain = new domains();
+							$domain->db = $db;
+							$domain->set();
 					}
 				}
 			}
@@ -121,7 +129,7 @@ session_start();
 						$_SESSION["time_zone"]["user"] = $row["user_time_zone"];
 					}
 				// add the user_uuid to the session
-					$_SESSION['user_uuid'] = $row['user_uuid'];
+					$_SESSION["user_uuid"] = $row["user_uuid"];
 			}
 
 		//get the groups assigned to the user and then set the groups in $_SESSION["groups"]
