75c5dd017583b523bce2cc889a44764ac771a892
Mark J Crane <markjcrane@fusionpbx.com>
2013-09-16 21:00:40 +0000

    Remove the svn artifacts.
    git-svn-id: http://fusionpbx.googlecode.com/svn/trunk@4489 e2954ef0-caed-11de-bd98-e341d93cc3cc

--- fusionpbx/app/dialplan_outbound/app_languages.php
+++ fusionpbx/app/dialplan_outbound/app_languages.php
@@ -27,375 +27,192 @@
 */
 
 	$text['title-dialplan-outbound-add']['en-us'] = 'Outbound Call Routing';
-<<<<<<< .mine
 	$text['title-dialplan-outbound-add']['fr'] = "Routage d'appel sortant";
-=======
 
->>>>>>> .r4473
 	$text['message-provide']['en-us'] = 'Please Provide';
-<<<<<<< .mine
 	$text['message-provide']['fr'] = "Merci d'indiquer";
-=======
 
->>>>>>> .r4473
 	$text['label-gateway-name']['en-us'] = 'Gateway Name';
-<<<<<<< .mine
 	$text['label-gateway-name']['fr'] = "Nom de la passerelle";
-=======
 
->>>>>>> .r4473
 	$text['label-dialplan-expression']['en-us'] = 'Dialplan Expression';
-<<<<<<< .mine
 	$text['label-dialplan-expression']['fr'] = "Expression du Plan de numérotation";
-=======
 
->>>>>>> .r4473
 	$text['label-2d']['en-us'] = '2 Digits';
-<<<<<<< .mine
 	$text['label-2d']['fr'] = '2 Digits';
-=======
 
->>>>>>> .r4473
 	$text['label-3d']['en-us'] = '3 Digits';
-<<<<<<< .mine
 	$text['label-3d']['fr'] = '3 Digits';
-=======
 
->>>>>>> .r4473
 	$text['label-4d']['en-us'] = '4 Digits';
-<<<<<<< .mine
 	$text['label-4d']['fr'] = '4 Digits';
-=======
 
->>>>>>> .r4473
 	$text['label-5d']['en-us'] = '5 Digits';
-<<<<<<< .mine
 	$text['label-5d']['fr'] = '5 Digits';
-=======
 
->>>>>>> .r4473
 	$text['label-6d']['en-us'] = '6 Digits';
-<<<<<<< .mine
 	$text['label-6d']['fr'] = '6 Digits';
-=======
 
->>>>>>> .r4473
 	$text['label-7d']['en-us'] = '7 Digits Local';
-<<<<<<< .mine
 	$text['label-7d']['fr'] = '7 Digits Local';
-=======
 
->>>>>>> .r4473
 	$text['label-8d']['en-us'] = '8 Digits';
-<<<<<<< .mine
 	$text['label-8d']['fr'] = '8 Digits';
-=======
 
->>>>>>> .r4473
 	$text['label-9d']['en-us'] = '9 Digits';
-<<<<<<< .mine
 	$text['label-9d']['fr'] = '9 Digits';
-=======
 
->>>>>>> .r4473
 	$text['label-10d']['en-us'] = '10 Digits Long Distance';
-<<<<<<< .mine
 	$text['label-10d']['fr'] = '10 Digits';
-=======
 
->>>>>>> .r4473
 	$text['label-11d']['en-us'] = '11 Digits Long Distance';
-<<<<<<< .mine
 	$text['label-11d']['fr'] = '11 Digits';
-=======
 
->>>>>>> .r4473
 	$text['label-12d']['en-us'] = '12 Digits';
-<<<<<<< .mine
 	$text['label-12d']['fr'] = '12 Digits';
-=======
 
->>>>>>> .r4473
 	$text['label-13d']['en-us'] = '13 Digits';
-<<<<<<< .mine
 	$text['label-13d']['fr'] = '13 Digits';
-=======
 
->>>>>>> .r4473
 	$text['label-14d']['en-us'] = '14 Digits';
-<<<<<<< .mine
 	$text['label-14d']['fr'] = '14 Digits';
-=======
 
->>>>>>> .r4473
 	$text['label-int']['en-us'] = '15 Digits International';
-<<<<<<< .mine
 	$text['label-int']['fr'] = '15 Digits';
-=======
 
->>>>>>> .r4473
 	$text['label-311']['en-us'] = '311 Information';
-<<<<<<< .mine
 	$text['label-311']['fr'] = '311 Information';
-=======
 
->>>>>>> .r4473
 	$text['label-411']['en-us'] = '411 Information';
-<<<<<<< .mine
 	$text['label-411']['fr'] = '411 Information';
-=======
 
->>>>>>> .r4473
 	$text['label-911']['en-us'] = '911 Emergency';
-<<<<<<< .mine
 	$text['label-911']['fr'] = '911 Emergency';
-=======
 
->>>>>>> .r4473
 	$text['label-9d2']['en-us'] = 'Dial 9, then 2 Digits';
-<<<<<<< .mine
 	$text['label-9d2']['fr'] = 'Compose 9, puis 2 Digits';
-=======
 
->>>>>>> .r4473
 	$text['label-9d3']['en-us'] = 'Dial 9, then 3 Digits';
-<<<<<<< .mine
 	$text['label-9d3']['fr'] = 'Compose 9, puis 3 Digits';
-=======
 
->>>>>>> .r4473
 	$text['label-9d4']['en-us'] = 'Dial 9, then 4 Digits';
-<<<<<<< .mine
 	$text['label-9d4']['fr'] = 'Compose 9, puis 4 Digits';
-=======
 
->>>>>>> .r4473
 	$text['label-9d5']['en-us'] = 'Dial 9, then 5 Digits';
-<<<<<<< .mine
 	$text['label-9d5']['fr'] = 'Compose 9, puis 5 Digits';
-=======
 
->>>>>>> .r4473
 	$text['label-9d6']['en-us'] = 'Dial 9, then 6 Digits';
-<<<<<<< .mine
 	$text['label-9d6']['fr'] = 'Compose 9, puis 6 Digits';
-=======
 
->>>>>>> .r4473
 	$text['label-9d7']['en-us'] = 'Dial 9, then 7 Digits';
-<<<<<<< .mine
 	$text['label-9d7']['fr'] = 'Compose 9, puis 7 Digits';
-=======
 
->>>>>>> .r4473
 	$text['label-9d8']['en-us'] = 'Dial 9, then 8 Digits';
-<<<<<<< .mine
 	$text['label-9d8']['fr'] = 'Compose 9, puis 8 Digits';
-=======
 
->>>>>>> .r4473
 	$text['label-9d9']['en-us'] = 'Dial 9, then 9 Digits';
-<<<<<<< .mine
 	$text['label-9d9']['fr'] = 'Compose 9, puis 9 Digits';
-=======
 
->>>>>>> .r4473
 	$text['label-9d10']['en-us'] = 'Dial 9, then 10 Digits';
-<<<<<<< .mine
 	$text['label-9d10']['fr'] = 'Compose 9, puis 10 Digits';
-=======
 
->>>>>>> .r4473
 	$text['label-9d11']['en-us'] = 'Dial 9, then 11 Digits';
-<<<<<<< .mine
 	$text['label-9d11']['fr'] = 'Compose 9, puis 11 Digits';
-=======
 
->>>>>>> .r4473
 	$text['label-9d12']['en-us'] = 'Dial 9, then 12 Digits';
-<<<<<<< .mine
 	$text['label-9d12']['fr'] = 'Compose 9, puis 12 Digits';
-=======
 
->>>>>>> .r4473
 	$text['label-9d13']['en-us'] = 'Dial 9, then 13 Digits';
-<<<<<<< .mine
 	$text['label-9d13']['fr'] = 'Compose 9, puis 13 Digits';
-=======
 
->>>>>>> .r4473
 	$text['label-9d14']['en-us'] = 'Dial 9, then 14 Digits';
-<<<<<<< .mine
 	$text['label-9d14']['fr'] = 'Compose 9, puis 14 Digits';
-=======
 
->>>>>>> .r4473
 	$text['label-9d15']['en-us'] = 'Dial 9, then International';
-<<<<<<< .mine
 	$text['label-9d15']['fr'] = 'Compose 9, puis International';
-=======
 
->>>>>>> .r4473
 	$text['label-800']['en-us'] = 'Toll-Free';
-<<<<<<< .mine
 	$text['label-800']['fr'] = 'Sans Charge';
-=======
 
->>>>>>> .r4473
 	$text['message-update']['en-us'] = 'Update Complete';
-<<<<<<< .mine
 	$text['message-update']['fr'] = "Mis à jour";
-=======
 
->>>>>>> .r4473
 	$text['label-outbound-routes']['en-us'] = 'Outbound Routes';
-<<<<<<< .mine
 	$text['label-outbound-routes']['fr'] = 'Routes Sortantes';
-=======
 
->>>>>>> .r4473
 	$text['button-back']['en-us'] = 'Back';
-<<<<<<< .mine
 	$text['button-back']['fr'] = 'Retour';
-=======
 
->>>>>>> .r4473
 	$text['description-outbound-routes']['en-us'] = 'Outbound dialplans have one or more conditions that are matched to attributes of a call.  When a call matches the conditions the call is then routed to the gateway.';
-<<<<<<< .mine
 	$text['description-outbound-routes']['fr'] = "les Plan de numérotation sortant doivent remplir une ou plusieures conditions afin d'acheminer un appel. Lorsqu'un appel rempli la ou des conditions, l'appel est alors acheminé à la passerelle.";
-=======
 
->>>>>>> .r4473
 	$text['label-gateway']['en-us'] = 'Gateway';
-<<<<<<< .mine
 	$text['label-gateway']['fr'] = 'Passerelle';
-=======
 
->>>>>>> .r4473
 	$text['label-sip-gateway']['en-us'] = 'SIP Gateways';
-<<<<<<< .mine
 	$text['label-sip-gateway']['fr'] = 'Passerelle SIP';
-=======
 
->>>>>>> .r4473
 	$text['label-add-options']['en-us'] = 'Additional Options';
-<<<<<<< .mine
 	$text['label-add-options']['fr'] = 'Autres Options';
-=======
 
->>>>>>> .r4473
 	$text['message-add-options']['en-us'] = 'Select the gateway to use with this outbound route.';
-<<<<<<< .mine
 	$text['message-add-options']['fr'] = "Choisir la passerelle utilisée par cette route sortante.";
-=======
 
->>>>>>> .r4473
 	$text['label-alt1']['en-us'] = 'Alternate 1';
-<<<<<<< .mine
 	$text['label-alt1']['fr'] = 'Alternative 1';
-=======
 
->>>>>>> .r4473
 	$text['message-add-options1']['en-us'] = 'Select another gateway as an alternative to use if the first one fails.';
-<<<<<<< .mine
 	$text['message-add-options1']['fr'] = "Choisir une autre passerelle comme alternative si la première échoue.";
-=======
 
->>>>>>> .r4473
 	$text['label-alt2']['en-us'] = 'Alternate 2';
-<<<<<<< .mine
 	$text['label-alt2']['fr'] = 'Alternative 2';
-=======
 
->>>>>>> .r4473
 	$text['message-add-options2']['en-us'] = 'Select another gateway as an alternative to use if the second one fails.';
-<<<<<<< .mine
 	$text['message-add-options2']['fr'] = "Choisir une autre passerelle comme alternative si la seconde échoue.";
-=======
 
->>>>>>> .r4473
 	$text['description-shortcut']['en-us'] = 'Shortcut to create the outbound dialplan entries for this Gateway.';
-<<<<<<< .mine
 	$text['description-shortcut']['fr'] = "Raccourci pour créer l'entrée de cette passerelle dans le plan de numérotation sortant.";
-=======
 
->>>>>>> .r4473
 	$text['label-prefix']['en-us'] = 'Prefix';
-<<<<<<< .mine
 	$text['label-prefix']['fr'] = 'Préfixe';
-=======
 
->>>>>>> .r4473
 	$text['description-enter-prefix']['en-us'] = 'Enter a prefix number to add to the beginning of the destination number.';
-<<<<<<< .mine
 	$text['description-enter-prefix']['fr'] = "Choisir un préfixe à ajouter en début de numérotation pour joindre la destination.";
-=======
 
->>>>>>> .r4473
 	$text['label-limit']['en-us'] = 'Limit';
-<<<<<<< .mine
 	$text['label-limit']['fr'] = 'Limite';
-=======
 
->>>>>>> .r4473
 	$text['description-limit']['en-us'] = 'Enter limit to restrict the number of outbound calls.';
-<<<<<<< .mine
 	$text['description-limit']['fr'] = "Choisir une limite afin de limiter le noombre d'appel sortants.";
-=======
 
 	$text['label-accountcode']['en-us'] = 'Account Code';
 
 	$text['description-accountcode']['en-us'] = 'Enter the accountcode.';
 
->>>>>>> .r4473
 	$text['label-order']['en-us'] = 'Order';
-<<<<<<< .mine
 	$text['label-order']['fr'] = 'Ordre';
-=======
 
->>>>>>> .r4473
 	$text['description-order']['en-us'] = 'Select the order number. The order number determines the order of the outbound routes when there is more than one.';
-<<<<<<< .mine
 	$text['description-order']['fr'] = "Choisir le numéro d'ordre. Ce nombre détermine l'ordre dans le quel cette route sera utilisées si plusieurs routes correspondant aux conditions de l'appel existent.";
-=======
 
->>>>>>> .r4473
 	$text['label-enabled']['en-us'] = 'Enabled';
-<<<<<<< .mine
 	$text['label-enabled']['fr'] = 'Actif';
-=======
 
->>>>>>> .r4473
 	$text['label-true']['en-us'] = 'True';
-<<<<<<< .mine
 	$text['label-true']['fr'] = 'Oui';
-=======
 
->>>>>>> .r4473
 	$text['label-false']['en-us'] = 'False';
-<<<<<<< .mine
 	$text['label-false']['fr'] = 'Non';
-=======
 
->>>>>>> .r4473
 	$text['description-enabled']['en-us'] = 'Choose to enable or disable the outbound route.';
-<<<<<<< .mine
 	$text['description-enabled']['fr'] = "";
-=======
 
->>>>>>> .r4473
 	$text['label-description']['en-us'] = 'Description';
-<<<<<<< .mine
 	$text['label-description']['fr'] = 'Description';
-=======
 
->>>>>>> .r4473
 	$text['description-description']['en-us'] = 'Enter a description for the outbound route.';
-<<<<<<< .mine
 	$text['description-description']['fr'] = "";
-=======
 
->>>>>>> .r4473
 	$text['button-save']['en-us'] = 'Save';
 	$text['button-save']['fr'] = 'Sauvegarder';
 
