a28844e6bec1621642a8e0275706cfb58db89b37
Mark J Crane <markjcrane@fusionpbx.com>
2013-11-03 05:20:08 +0000

    Fix the regular expression for the outbound route for North America and International.
    git-svn-id: http://fusionpbx.googlecode.com/svn/trunk@4679 e2954ef0-caed-11de-bd98-e341d93cc3cc

--- fusionpbx/app/dialplan_outbound/dialplan_outbound_add.php
+++ fusionpbx/app/dialplan_outbound/dialplan_outbound_add.php
@@ -278,7 +278,7 @@ else {
 						$label = $text['label-north-america'];
 						$abbrv = "10-11d";
 						break;
-					case "^(011(\d{9,17})$":
+					case "^(011\d{9,17})$":
 						$label = $text['label-north-america-intl'];
 						$abbrv = "011.9-17d";
 						break;
@@ -867,7 +867,7 @@ function type_onchange(dialplan_detail_type) {
 	echo "    <option value='^(\\d{9})\$'>".$text['label-9d']."</option>\n";
 	echo "    <option value='^(\\d{10})\$'>".$text['label-10d']."</option>\n";
 	echo "    <option value='^\+?(\\d{11})\$'>".$text['label-11d']."</option>\n";
-	echo "    <option value='^(\+?1?(\\d{10})\$'>".$text['label-north-america']."</option>\n";
+	echo "    <option value='^\+?1?(\\d{10})\$'>".$text['label-north-america']."</option>\n";
 	echo "    <option value='^(011\\d{9,17})\$'>".$text['label-north-america-intl']."</option>\n";
 	echo "    <option value='^(\\d{12,20})\$'>".$text['label-intl']."</option>\n";
 	echo "    <option value='^(311)\$'>".$text['label-311']."</option>\n";
