67e5dcf3cf55328f5c616d4c03b3340206ab2a7c
Mark J Crane <markjcrane@fusionpbx.com>
2014-01-16 19:31:08 +0000

    Set the outbound routes default order to 100.
    git-svn-id: http://fusionpbx.googlecode.com/svn/trunk@5090 e2954ef0-caed-11de-bd98-e341d93cc3cc

--- fusionpbx/app/dialplan_outbound/dialplan_outbound_add.php
+++ fusionpbx/app/dialplan_outbound/dialplan_outbound_add.php
@@ -17,7 +17,7 @@
 
 	The Initial Developer of the Original Code is
 	Mark J Crane <markjcrane@fusionpbx.com>
-	Portions created by the Initial Developer are Copyright (C) 2008-2012
+	Portions created by the Initial Developer are Copyright (C) 2008-2014
 	the Initial Developer. All Rights Reserved.
 
 	Contributor(s):
@@ -48,7 +48,7 @@ else {
 	require_once "resources/paging.php";
 
 //get the http post values and set theme as php variables
-	if (count($_POST)>0) {
+	if (count($_POST) > 0) {
 		//set the variables
 			$dialplan_name = check_str($_POST["dialplan_name"]);
 			$dialplan_order = check_str($_POST["dialplan_order"]);
@@ -639,7 +639,6 @@ function type_onchange(dialplan_detail_type) {
 	echo "<br />\n";
 
 	echo "<table width='100%'  border='0' cellpadding='6' cellspacing='0'>\n";
-
 	echo "<tr>\n";
 	echo "<td class='vncellreq' valign='top' align='left' nowrap>\n";
 	echo "    ".$text['label-gateway'].":\n";
@@ -927,24 +926,24 @@ function type_onchange(dialplan_detail_type) {
 
 	echo "<tr>\n";
 	echo "<td class='vncellreq' valign='top' align='left' nowrap>\n";
-	echo "    ".$text['label-order'].":\n";
+	echo "	".$text['label-order'].":\n";
 	echo "</td>\n";
 	echo "<td class='vtable' align='left'>\n";
-	echo "              <select name='dialplan_order' class='formfld' style='width: 60%;'>\n";
-	//echo "              <option></option>\n";
+	echo "	<select name='dialplan_order' class='formfld' style='width: 60%;'>\n";
+	//echo "		<option></option>\n";
 	if (strlen(htmlspecialchars($dialplan_order))> 0) {
-		echo "              <option selected='yes' value='".htmlspecialchars($dialplan_order)."'>".htmlspecialchars($dialplan_order)."</option>\n";
+		echo "		<option selected='yes' value='".htmlspecialchars($dialplan_order)."'>".htmlspecialchars($dialplan_order)."</option>\n";
 	}
-	$i=0;
-	while($i<=999) {
-		if (strlen($i) == 1) { echo "              <option value='00$i'>00$i</option>\n"; }
-		if (strlen($i) == 2) { echo "              <option value='0$i'>0$i</option>\n"; }
-		if (strlen($i) == 3) { echo "              <option value='$i'>$i</option>\n"; }
-		$i++;
+	$i = 100;
+	while($i <= 999) {
+		if (strlen($i) == 1) { echo "		<option value='00$i'>00$i</option>\n"; }
+		if (strlen($i) == 2) { echo "		<option value='0$i'>0$i</option>\n"; }
+		if (strlen($i) == 3) { echo "		<option value='$i'>$i</option>\n"; }
+		$i = $i + 10;
 	}
-	echo "              </select>\n";
-	echo "<br />\n";
-	echo $text['description-order']."\n";
+	echo "	</select>\n";
+	echo "	<br />\n";
+	echo "	".$text['description-order']."\n";
 	echo "</td>\n";
 	echo "</tr>\n";
 
