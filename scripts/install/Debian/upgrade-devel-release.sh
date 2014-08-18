c327fd63f10069d3160579e82af61b474cba1359
Richard Neese <r.neese@gmail.com>
2014-08-18 02:17:46 +0000

    git-svn-id: http://fusionpbx.googlecode.com/svn/trunk@6724 e2954ef0-caed-11de-bd98-e341d93cc3cc

--- scripts/install/Debian/upgrade-devel-release.sh
+++ scripts/install/Debian/upgrade-devel-release.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
+#change repo to release repo
+cat > /etc/apt/sources.list.d/fusionpbx.list << DELIM
+deb http://repo.fusionpbx.com/deb/debian/ wheezy main
+DELIM
+#update repo list
+apt-get update
+#read pkg out to a logfile
 dpkg --get-selections 'fusionpbx*' > /tmp/fusionpbx-pkg.log
+#remove the pkgs in the list
 apt-get remove -y fusionpbx*
+#read list and reinstall rm pkgs
 aptitude install $(cat /tmp/fusionpbx-pkg.log | awk '{print $1}')
+#change to the fusionpbx www dir
 cd /usr/share/nginx/www/fusionpbx
+#run upgrade commands
 php /usr/share/nginx/www/fusionpbx/core/upgrade/upgrade.php
 php /usr/share/nginx/www/fusionpbx/core/upgrade/upgrade_domains.php
 php /usr/share/nginx/www/fusionpbx/core/upgrade/app_defaults.php
 php /usr/share/nginx/www/fusionpbx/core/upgrade/upgrade_schema.php
+#cd root dir
 cd ~
