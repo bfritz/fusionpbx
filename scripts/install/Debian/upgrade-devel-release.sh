60d440a808295c08250c20c7d9edc13cc8520478
Richard Neese <r.neese@gmail.com>
2014-08-21 13:51:43 +0000

    git-svn-id: http://fusionpbx.googlecode.com/svn/trunk@6741 e2954ef0-caed-11de-bd98-e341d93cc3cc

--- scripts/install/Debian/upgrade-devel-release.sh
+++ scripts/install/Debian/upgrade-devel-release.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+ste#!/bin/bash
 #change repo to release repo
 cat > /etc/apt/sources.list.d/fusionpbx.list << DELIM
 deb http://repo.fusionpbx.com/deb/debian/ wheezy main
@@ -20,4 +20,8 @@ php /usr/share/nginx/www/fusionpbx/core/upgrade/app_defaults.php
 php /usr/share/nginx/www/fusionpbx/core/upgrade/upgrade_schema.php
 #cd root dir
 cd ~
+cp -rp /var/lib/fusionpbx/scripts /var/lib/fusionpbx/scripts.bak
+rm -rf /var/lib/fusionpbx/scripts/*
+cp -rp /usr/share/fusionpbx/resources/install/scripts/* /var/lib/fusionpbx/scripts
+chown -R www-data:www-data /var/lib/fusionpbx/scripts
 apt-get remove custom-scripts && apt-get install custom-scripts
