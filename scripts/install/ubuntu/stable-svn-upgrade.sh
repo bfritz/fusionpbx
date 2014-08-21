7e2ec64df440989df3292c9698b20b380fbedefb
Richard Neese <r.neese@gmail.com>
2014-08-21 13:27:22 +0000

    new upgrade script for those using soapies script to move from dev branch to stable branch
    git-svn-id: http://fusionpbx.googlecode.com/svn/trunk@6737 e2954ef0-caed-11de-bd98-e341d93cc3cc

---
+++ scripts/install/ubuntu/stable-svn-upgrade.sh
@@ -0,0 +1,15 @@
+#!bin/bash
+svn swap http://fusionpbx.googlecode.com/svn/trunk/fusionpbx
+#change to the fusionpbx www dir
+cd /var/www/fusionpbx
+svn update
+#run upgrade commands
+php /var/www/fusionpbx/core/upgrade/upgrade.php
+php /var/www/fusionpbx/core/upgrade/upgrade_domains.php
+php /var/www/fusionpbx/core/upgrade/app_defaults.php
+php /var/www/fusionpbx/core/upgrade/upgrade_schema.php
+#cd root dir
+cd ~
+rm -rf /usr/local/freeswitch/scripts/*
+cp -r /var/www/fusionpbx/resources/install/scripts/* /usr/local/freeswitch/scripts
+chown -R www-data:www-data /usr/local/freeswitch/scripts
