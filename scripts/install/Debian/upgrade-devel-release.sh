fbdef7a7e6d5e5125bb8d0c50591a2062e4e285f
Richard Neese <r.neese@gmail.com>
2014-09-27 15:12:36 +0000

    git-svn-id: http://fusionpbx.googlecode.com/svn/trunk@6896 e2954ef0-caed-11de-bd98-e341d93cc3cc

--- scripts/install/Debian/upgrade-devel-release.sh
+++ scripts/install/Debian/upgrade-devel-release.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #change repo to release repo
 cat > /etc/apt/sources.list.d/fusionpbx.list << DELIM
-deb http://repo.fusionpbx.com/deb/debian/ wheezy main
+deb http://91.121.162.77/deb/debian/ wheezy main
 DELIM
-
+##
 case $(uname -m) in armv7l)
 /bin/cat > "/etc/apt/sources.list.d/voyagepbx.list" <<DELIM
 deb http://91.121.162.77/deb-stable/debian/ wheezy main
 DELIM
 esac
-
+##
 #update repo list
 apt-get update
 #read pkg out to a logfile
@@ -27,13 +27,17 @@ php /usr/share/nginx/www/fusionpbx/core/upgrade/app_defaults.php
 php /usr/share/nginx/www/fusionpbx/core/upgrade/upgrade_schema.php
 #cd root dir
 cd ~
+#update scripts
 cp -rp /var/lib/fusionpbx/scripts /var/lib/fusionpbx/scripts.bak
 rm -rf /var/lib/fusionpbx/scripts/*
 cp -rp /usr/share/fusionpbx/resources/install/scripts /var/lib/fusionpbx/
 chown -R www-data:www-data /var/lib/fusionpbx/scripts
 find "/var/lib/fusionpbx/scripts" -type f -exec chmod 664 {} +
 find "/var/lib/fusionpbx/scripts" -type d -exec chmod 775 {} +
+#update custom shell script menu
 apt-get remove custom-scripts && apt-get install custom-scripts
 #fix faxing recv dir
 find "/var/lib/freeswitch/storage" -type f -exec chmod 664 {} +
 find "/var/lib/freeswitch/storage" -type d -exec chmod 775 {} +
+#temp fix
+ln -s /usr/share/examples/fusionpbx /usr/share/fusionpbx
