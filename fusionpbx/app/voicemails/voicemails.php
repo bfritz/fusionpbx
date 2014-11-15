a5c7ba0e0cad9b34206ef7e5dc78815cc8454d14
Mark J Crane <markjcrane@fusionpbx.com>
2014-11-15 00:39:12 +0000

    Change $value to $row on trunk.
    git-svn-id: http://fusionpbx.googlecode.com/svn/trunk@7232 e2954ef0-caed-11de-bd98-e341d93cc3cc

--- fusionpbx/app/voicemails/voicemails.php
+++ fusionpbx/app/voicemails/voicemails.php
@@ -40,8 +40,8 @@ else {
 	}
 
 //set the voicemail_id array
-	foreach ($_SESSION['user']['extension'] as $value) {
-		$voicemail_ids[]['voicemail_id'] = $value['user'];
+	foreach ($_SESSION['user']['extension'] as $row) {
+		$voicemail_ids[]['voicemail_id'] = $row['user'];
 		if (strlen($row['number_alias']) > 0) {
 			$voicemail_ids[]['voicemail_id'] = $row['number_alias'];
 		}
