6a5cf53183f16e02de272db0bd675a903eb9f817
Richard Neese <r.neese@gmail.com>
2015-01-27 19:58:29 +0000

    git-svn-id: http://fusionpbx.googlecode.com/svn/trunk@7758 e2954ef0-caed-11de-bd98-e341d93cc3cc

--- scripts/install/Debian/fix-release-repo-arm-debian.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cat > "/etc/apt/sources.list.d/freeswitch.list" <<DELIM
-deb http://repo.fusionpbx.com/freeswitch-armhf/release/debian/ wheezy main
-DELIM
-
-cat > "/etc/apt/sources.list.d/fusionpbx.list" << DELIM
-deb http://repo.fusionpbx.com/fusionpbx/release/debian/ wheezy main
-DELIM
--- scripts/install/Debian/fix-release-repo-debian.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-/bin/cat > "/etc/apt/sources.list.d/freeswitch.list" <<DELIM
-deb http://repo.fusionpbx.com/freeswitch/release/debian/ wheezy main
-DELIM
-
-cat > "/etc/apt/sources.list.d/fusionpbx.list" << DELIM
-deb http://repo.fusionpbx.com/fusionpbx/release/debian/ wheezy main
-DELIM
